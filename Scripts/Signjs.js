ef411823c316675b4ecff370f33a8a8d3b42058d
yennhi-1107 <akyomin1107@gmail.com>
2023-10-30 18:50:19 +0700

    first commit

---
+++ Scripts/Signjs.js
@@ -0,0 +1,10 @@
+﻿document.getElementById("form1-fieldset").addEventListener("basicInfo", function (event) {
+    event.preventDefault();
+var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (password false) {
+        alert("Mật khẩu không đúng");
+        return;
+}
+else
+http://nhanam.com.vn/
---
+++ Views/Home/Sign.vbhtml
@@ -0,0 +1,7 @@
+﻿@Code
+    ViewData("Title") = "Đăng nhập | Nhã Nam"
+    Layout = "~/Views/Shared/_DngnhapPage1.vbhtml"
+End Code
+
+
+
